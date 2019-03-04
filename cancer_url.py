7f168b6270150627b8e9bebde9bfde669e528dcd
palooney <palooneylee@gmail.com>
2019-03-03 22:34:39 -0600

    extracting url by leading character

--- cancer_url.py
+++ cancer_url.py
@@ -1,44 +1,58 @@
-import requests, time
-from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import csv
 ## extracting url for each 
 class cancer_url(object):
 	def __init__(self):
 		self.urls = {}
 		self.domain = "https://www.mskcc.org/cancer-care"
 		self.start_page = "https://www.mskcc.org/cancer-care/diagnosis-treatment/symptom-management/integrative-medicine/herbs/search"
+		## pages that are split by herb leading character
+		self.pages = {}
+	## setup selenium webdriver
+	def driverSetup(self):
+		options = Options()
+		## do not open firefox 
+		options.add_argument("--headless")
+		driver = webdriver.Firefox(executable_path = "/usr/local/bin/geckodriver", options = options)
+		return driver
+	## iterate by leading character
+	def loadKeyword(self, driver):
+		driver.get(self.start_page)
+		driver.implicitly_wait(10)
+		element =  driver.find_elements_by_class_name("form-keyboard-letter")
+		for each in element:
+			url = each.get_attribute("href")
+			if url.startswith("https://www.mskcc.org/cancer-care"):
+				self.pages[each.text] = url
+			else:
+				url = self.domain + url
+				self.pages[each.text] = url
+		self.write()
+		driver.close()
+	## write urls to file
+	def write(self):
+		print("start writing leading character specific website into file")
+		with open("cancer_url.csv", "w") as f:
+			w = csv.writer(f)
+			w.writerows(self.pages.items())
+		print("Finished!")
 	## load entire page 
 	def loadMore(self):
 		driver = webdriver.Firefox(executable_path = "/usr/local/bin/geckodriver")
 		driver.get(self.start_page)
 		#html = driver.page_source.encode("utf-8")
-		while driver.find_elements_by_css_selector(".pager__item"):
-			driver.find_elements_by_css_selector(".pager__item").click()
-			time.sleep(1)
-		
-	def start(self):
-		page = requests.get(self.start_page)
-		soup = bs(page.text, "lxml")
-		return soup
-	## find url for each herb
-	def findHerb(self, soup):
-		context = soup.find("msk-view-content")
-		herbs = context.findAll("h2")
-		for each in herbs:
-			herb = each["a"]
-			print(herb.text.strip(), herb["href"])
-	## main function
-	def run(self):
-		self.loadMore()
-		soup = self.start()
-		self.findHerb(soup)
+		while driver.find_element_by_link_text("Load More"):
+			driver.find_element_by_link_text("Load More").click()
+		print("Finish clicking")
 
-
+x = cancer_url()
+driver = x.driverSetup()
+x.loadKeyword(driver)
 
 
 
 
 
 x = cancer_url()
-x.run()
 

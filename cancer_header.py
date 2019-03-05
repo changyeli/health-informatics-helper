68b56cd515079a68c53828912164aca447344404
palooney <palooneylee@gmail.com>
2019-03-04 21:46:30 -0600

    extracting headers w/o h2

--- cancer_header.py
+++ cancer_header.py
@@ -5,62 +5,43 @@ from collections import Counter
 class cancer_header(object):
 	def __init__(self):
 		self.herb = "cancer_herb_url.csv"
-		## headers for patients & caregivers
-		self.con = []
-		## headers for professionals
-		self.pro = []
+		## header names
+		self.headers = []
 	def driverSetup(self):
 		options = Options()
 		## do not open firefox 
 		options.add_argument("--headless")
 		driver = webdriver.Firefox(executable_path = "/usr/local/bin/geckodriver", options = options)
 		return driver
-	## extract all headers for patients & caregivers
-	def conSection(self, driver):
-		print("Extracting patients & caregivers")
-		element = driver.find_element_by_id("msk_consumer")
-		headers = element.find_elements_by_xpath('.//span[@class = "accordion__headline"]')
-		print(headers)
-		for each in headers:
-			name = each.get_attribute("data-listname")
-			print(name)
-			#self.con.append(name.strip())
-	## extract all headers for professionals
-	def proSection(self, driver):
+	## extract all headers
+	def extractHeader(self, driver):
 		print("Extracting professionals")
-		element = driver.find_element_by_id("msk_professional")
-		headers = element.find_elements_by_xpath('.//span[@class = "accordion__headline"]')
-		print(headers)
+		headers = driver.find_elements_by_class_name("accordion__headline")
 		for each in headers:
 			name = each.get_attribute("data-listname")
-			print(name)
-			#self.pro.append(name.strip())
+			self.headers.append(name.strip())
 	## write to files
 	def write(self):
 		print("Writing to files....")
-		with open("cancer_consumer.csv", "w") as f:
+		with open("cancer_herb_header.csv", "w") as f:
 			w = csv.writer(f)
-			w.writerows(Counter(self.con).items())
-		with open("cancer_professional.csv", "w") as f:
-			w = csv.writer(f)
-			w.writerows(Counter(self.pro).items())
+			w.writerows(Counter(self.headers).items())
 		print("Finish writing")
-	def process(self):
-		driver = self.driverSetup()
-		driver.implicitly_wait(1)
-		with open("cancer_herb_url.csv", "r") as f:
-			readCSV = csv.reader(f, delimiter = ",")
-			for row in readCSV:
-				print("Extracting " + row[0] + " headers")
-				driver.get(row[1])
-				self.conSection(driver)
-				self.proSection(driver)
-				print("=====================================")
-				break
-		#self.write()
 	## main function
 	def run(self):
-		self.process()
+		driver = self.driverSetup()
+		driver.implicitly_wait(1)
+		try:
+			with open("cancer_herb_url.csv", "r") as f:
+				readCSV = csv.reader(f, delimiter = ",")
+				for row in readCSV:
+					print("Extracting " + row[0] + " headers")
+					driver.get(row[1])
+					self.extractHeader(driver)
+					print("=====================================")
+			self.write()
+		except IOError:
+			print("No such file, please run cancer_url.py first")
 
 x = cancer_header()
 x.run()

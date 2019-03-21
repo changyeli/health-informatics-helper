d640bce98351ec1449e7694c1619b6c108734144
palooney <palooneylee@gmail.com>
2019-03-21 18:33:41 +0800

    add structure flags

--- CANCER/cancer_content_ann.py
+++ CANCER/cancer_content_ann.py
@@ -64,52 +64,102 @@ class cancer_ann(object):
     ## @name: cancer herb name
     ## @ar: adverse reactions content
     ## @filename: local file name to store annotated adverse reactions
-    def writeADR(self, ar_labels, name, ar, filename):
+    def writeADR(self, ar_labels, ar):
+        data = {}
         ## have content under the section
         if ar_labels is not None:
             data = {}
-            data["name"] = name
             data["ar_label"] = ar_labels
-            data["ar_context"] = ar
-            with open(filename, "a") as output2:
-                json.dump(data, output2)
-                output2.write("\n")
+            if not ar:
+                data["ar_context"] = "None"
+            else:
+                data["ar_context"] = self.concate(ar)
         else:
             ## no available annotation
-            data = {}
-            data["name"] = name
             data["ar_label"] = "None"
-            data["ar_context"] = ar
-            with open(filename, "a") as output1:
-                json.dump(data, output1)
-                output1.write("\n")
+            if not ar:
+                data["ar_context"] = "None"
+            else:
+                data["ar_context"] = self.concate(ar)
+        return data
             
     ## write adverse reactions to local file
     ## @hdi_labels: annotated labels from hdiProcess function
     ## @name: cancer herb name
     ## @ar: adverse reactions content
     ## @filename: local file name to store annotated herb-drug interaction
-    def writeHDI(self, hdi_labels, name, hdi, filename):
+    def writeHDI(self, hdi_labels, hdi):
+        data = {}
         ## check if no such section
         if hdi_labels is not None:
-            data = {}
-            data["name"] = name
             data["hdi_label"] = hdi_labels
-            data["hdi_context"] = hdi
-            with open(filename, "a") as output2:
-                json.dump(data, output2)
-                output2.write("\n")
+            if not hdi:
+                data["hdi_context"] = "None"
+            else:
+                data["hdi_context"] = self.concate(hdi)
         ## check if there is available annotation
         else:
             ## no available annotation
-            data = {}
-            data["name"] = name
-            data["hdi_label"] = "None"
-            data["hdi_context"] = hdi
-            with open(filename, "a") as output1:
-                json.dump(data, output1)
-                output1.write("\n")
-            
+            data["hdi_label"] = hdi_labels
+            if not hdi:
+                data["hdi_context"] = "None"
+            else:
+                data["hdi_context"] = self.concate(hdi)
+        return data
+    ## write to local file
+    ## @data: dict that contains annotated content
+    ## @filename: local file to save data
+    def write(self, data, filename):
+        with open(filename, "a") as output2:
+            json.dump(data, output2)
+            output2.write("\n")   
+    ## check if the content is structure
+    ## @content: specific data content, i.e. herb["adverse_reactions"]
+    ## returns "yes" if the content has ":", otherwise returns "no"
+    def isStructure(self, content):
+        content = self.concate(content)
+        if ":" in content:
+            return "Yes"
+        else:
+            return "No"
+    ## check purposed uses content:
+    ## @content: herb["purposed_uses"]
+    ## return list of purposed uses
+    def checkPU(self, content):
+        if isinstance(content, list):
+            return content
+        else:
+            return content.split("\n")
+    ## check if purposed uses section is structure
+    ## @content: herb["purposed_uses"]
+    def isStructurePU(self, content):
+        if isinstance(content, list):
+            return "Yes"
+        else:
+            return "No"
+    ## merge all dict into one
+    ## @ar: herb["adverse_reactions"]
+    ## @hdi: herb["herb-drug_interactions"]
+    ## @pu: herb["purported_uses"]
+    ## @name: herb name
+    ## return dict which contains structure flags, hdi & ar annotation, original content, pu content
+    def merge(self, ar, hdi, pu, name):
+        ## get ar & hdi annotated
+        ar_label = self.adrProcess(ar)
+        hdi_label = self.hdiProcess(hdi)
+        ar_data = self.writeADR(ar_label, ar)
+        hdi_data = self.writeHDI(hdi_label, hdi)
+        ## merge into one dict
+        full = {**ar_data, **hdi_data} 
+        full["name"] = name
+        ## add ar & hdi flag
+        full["ar_flag"] = self.isStructure(ar)
+        full["hdi_flag"] = self.isStructure(hdi)
+        ## check purposed uses section
+        full["pu_context"] = self.checkPU(pu)
+        full["pu_flag"] = self.isStructurePU(pu)
+        return full
+
     ## read cancer herb content line by line
     def readJSON(self):
         try:
@@ -118,13 +168,12 @@ class cancer_ann(object):
                     data = json.loads(line)
                     hdi = data["herb-drug_interactions"]
                     ar = data["adverse_reactions"]
+                    pu = data["purported_uses"]
                     print("=========================")
                     name = data["name"]
                     print("processing: " + name)
-                    ar_labels = self.adrProcess(ar)
-                    hdi_labels = self.hdiProcess(hdi)
-                    self.writeADR(ar_labels, name, ar, "cancer_adr.json")
-                    self.writeHDI(hdi_labels, name, hdi, "cancer_hdi.json")
+                    full = self.merge(ar, hdi, pu, name)
+                    self.write(full, "cancer_ann.json")
                     print("=========================")
         except IOError:
             print("No such file. Please run cancer_context.py first.")
@@ -135,7 +184,7 @@ class cancer_ann(object):
         self.readJSON()
         print("finished....")
         endtime = time.time()
-        print("time used: " + str(endtime - start))
+        print("time used: " + str(endtime - start) + " second(s).")
 if __name__ == "__main__":
     x = cancer_ann()
     x.run()
--- CANCER/util.py
+++ CANCER/util.py
@@ -1,6 +1,6 @@
-import json
+import json, os.path
+import pandas as pd
 file = "cancer_herb_content.json"
-output = "cancer_sub_content.json"
 ## extract necessary subheaders
 def extract():
     try:
@@ -11,33 +11,89 @@ def extract():
                 name = herb["name"]
                 data["name"] = name
                 ## extract content for annotation
-                if "herb-drug_interactions" in herb:
-                    hdi = herb["herb-drug_interactions"]
-                    data["herb-drug_interactions"] = hdi
-                if "adverse_reactions" in herb:
-                    ar = herb["adverse_reactions"]
-                    data["adverse_reactions"] = ar
-                if "purposed_uses" in herb:
-                    pu = herb["purposed_uses"]
-                    data["purposed_uses"] = pu
-                ## if the herb has more than 1 desired element
-                if len(data) > 1:
-                    write(data)
+                if "purported_uses" in herb:
+                    pu = herb["purported_uses"]
+                    if isinstance(pu, list):
+                    	data["purported_uses"] = ",".join(pu)
+                    else:
+                    	pu = pu.split("\n")
+                    	data["purported_uses"] = ",".join(pu)
                 else:
-                    pass
+                	pass
+                write(data, "cancer_pu.json")
     except IOError:
         print("No such file. Please run cancer_context.py first.")
+## concatenate list of string into string
+def concate(value):
+	if isinstance(value, list):
+		return ",".join(value)
+	else:
+		return value
 ## write to local file
 ## @value: desired cancer herb content
-def write(value):
+def write(value, output):
     with open(output, "a") as f:
         json.dump(value, f)
         f.write("\n")
-## main function
+## read json file for adr & hdi
+## @json_file: json file to read. i.e cancer_adr.json
+## @output_file: output file for preprocessed herb
+## return herb
+def read(json_file, output_file):
+	with open(json_file, "r") as f:
+		for lines in f:
+			herb = {}
+			value = json.loads(lines)
+			for k, v in value.items():
+				## replace none to empty string
+				if v is None or v == "None":
+					herb[k] = " "
+				else:
+					herb[k] = concate(v)
+			write(herb, output_file)
+
+## merge into single dataframe
+## return merged dataframe
+def merge():
+	read("cancer_adr.json", "cancer_adr_mergerd.json")
+	read("cancer_hdi.json", "cancer_hdi_merged.json")
+	adr = pd.read_json("cancer_adr_mergerd.json", lines = True)
+	hdi = pd.read_json("cancer_hdi_merged.json", lines = True)
+	pu = pd.read_json("cancer_pu.json", lines = True)
+	content = adr.merge(hdi, on = "name")
+	content_full = content.merge(pu, on = "name")
+	return content_full.drop_duplicates()
+## add flags to the merged dataframe
+## write merged dataframe into local file
+## @data: merged dataframe which contains ar, hdi, labels and original content
+## @output_file: output file name
+## return dataframe that added flags
+def addFlag(data, output_file):
+	data["ar_flag"] = data["ar_context"].str.contains(":")
+	data["hdi_flag"] = data["hdi_context"].str.contains(":")
+	if os.path.exists(output_file):
+		pass
+	else:
+		data.to_csv(output_file, sep = "\t")
+	return data
+## compute basic statistics
+def stat(data):
+	print("Total %d Cancer herbs" %(data.shape[0]))
+	print("Total %d Cancer herbs have structured adverse reactions content." % data.loc[data["ar_flag"] == True].shape[0])
+	print("Total %d Cancer herbs have structured herb-drug interactions content." % data.loc[data["hdi_flag"] == True].shape[0])
+	print("Total %d Cancer herbs have both structured adverse reactions and herb-drug interactions content." % data.loc[(data["hdi_flag"] == True) & (data["ar_flag"] == True)].shape[0])
+	print("Total %d Cancer herbs do not have adverse reactions content" % data.loc[data["ar_context"] == " "].shape[0])
+	print("Total %d Cancer herbs do not have herb-drug interactions content" % data.loc[data["hdi_context"] == " "].shape[0])
 def run():
-    print("start")
-    extract()
-    print("finish")
+	if os.path.exists("cancer_pu.json"):
+		data = merge()
+		data = addFlag(data, "merged_cancer.csv")
+		stat(data)
+	else:
+		extract()
+		data = merge()
+		data = addFlag(data, "merged_cancer.csv")
+		stat(data)
 
 if __name__ == "__main__":
     run()

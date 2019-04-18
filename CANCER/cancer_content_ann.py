a8717cffb6ac682b1a89f298a2df48948c8f0102
palooney <palooneylee@gmail.com>
2019-04-17 22:12:33 -0500

    output selection update

--- CANCER/cancer_content_ann.py
+++ CANCER/cancer_content_ann.py
@@ -1,40 +1,48 @@
-import json, re, csv, os
+import json
+import re
+import csv
+import os
 from umlsAnn import umlsAnn
 from meddraAnn import meddraAnn
 from collections import Counter
+from operator import itemgetter 
 import pandas as pd
-## main class for getting annotation
+# main class for getting annotation
+
+
 class main(object):
     def __init__(self):
-        ## get annotation using bioportal for adverse reaction section
+        # get annotation using bioportal for adverse reaction section
         self.read_file = "cancer_herb_content.json"
-        ## this file location
+        # this file location
         self.file_location = os.path.dirname(os.path.realpath(__file__))
-        ## overlap ingredients
+        # overlap ingredients
         self.overlap_herbs = ["Andrographis", "Blue-Green Algae", "Bromelain", "Butterbur",
-                        "Calcium", "Cranberry", "Elderberry", "Fenugreek",
-                        "Flaxseed", "Folic Acid", "Ginkgo", "Grape",
-                        "Hops", "Indole-3-Carbinol", "Kudzu", "L-Arginine",
-                        "L-Tryptophan", "Melatonin", "N-Acetyl Cysteine", "Pomegranate",
-                        "Red Clover", "Reishi Mushroom", "Shiitake Mushroom", "Siberian Ginseng", 
-                        "Turmeric", "Vitamin B12", "Vitamin E", "Vitamin K",
-                        "Blue-green Algae", "Folate", "Grape seeds", "Arginine",
-                        "5-HTP", "N-Acetylcysteine"]
-        ## remove "herb-drug_interactions", "adverse_reactions", "purported_uses", "contraindications" for specific pre-processing
+                              "Calcium", "Cranberry", "Elderberry", "Fenugreek",
+                              "Flaxseed", "Folic Acid", "Ginkgo", "Grape",
+                              "Hops", "Indole-3-Carbinol", "Kudzu", "L-Arginine",
+                              "L-Tryptophan", "Melatonin", "N-Acetyl Cysteine", "Pomegranate",
+                              "Red Clover", "Reishi Mushroom", "Shiitake Mushroom", "Siberian Ginseng",
+                              "Turmeric", "Vitamin B12", "Vitamin E", "Vitamin K",
+                              "Blue-green Algae", "Folate", "Grape seeds", "Arginine",
+                              "5-HTP", "N-Acetylcysteine"]
+        # remove "herb-drug_interactions", "adverse_reactions", "purported_uses", "contraindications" for specific pre-processing
         self.headers = ["last_updated", "common_name", "scientific_name",
-                    "warnings",  "clinical_summary",
-                "food_sources", "mechanism_of_action"]
-    ## concate list of string into single string
-    ## @value: content to be concated
-    ## @sep: separator, i.e. "\t", "\n", " "
+                        "warnings", "clinical_summary",
+                        "food_sources", "mechanism_of_action"]
+    # concate list of string into single string
+    # @value: content to be concated
+    # @sep: separator, i.e. "\t", "\n", " "
+
     def concate(self, value, sep):
         if isinstance(value, list):
             return (sep.join(value))
         else:
             return value
-    ## remove all non-ASCII characters in the content
-    ## remove contents inside of ()
-    ## @value: the content needs to be cleaned
+    # remove all non-ASCII characters in the content
+    # remove contents inside of ()
+    # @value: the content needs to be cleaned
+
     def remove(self, value):
         if isinstance(value, list):
             value = [re.sub(r'[^\x00-\x7F]+', " ", each) for each in value]
@@ -44,81 +52,109 @@ class main(object):
             value = re.sub(r'[^\x00-\x7F]+', " ", value)
             value = re.sub(r" ?\([^)]+\)", "", value)
             return value
-    ## get content before ":"
-    ## @content: content need to be split 
+    # get content before ":"
+    # @content: content need to be split
+    # return a list of contents that are before ":"
+
     def getBefore(self, content):
         if isinstance(content, list):
             value = [each.split(":")[0] for each in content]
             return value
         else:
-            return content.split(":")[0]
-    ## write to local .tsv file if the content exists
-    ## @output_file: file to write
-    ## @data: extracted herb information in dict form
+            value = content.split("\n")
+            value = [each.split(":")[0] for each in value]
+            return value
+    # write to local .tsv file if the content exists
+    # @output_file: file to write
+    # @data: extracted herb information in dict form
+
     def writeContent(self, output_file, data):
         with open(os.path.join(self.file_location, output_file), "a") as output:
-            w = csv.writer(output, delimiter = "\t")
+            w = csv.writer(output, delimiter="\t")
             w.writerow([v for v in data.values()])
-    ## process MM JSON output
-    ## @output: MM output, string type
-    ## @types: list of semantic types
-    ## return the max scored item
-    ## if it has more than one max scored item, return the item with more MM types
-    ## TODO: need to rewrite this part to avoid nested loop
+
+    # check if a list is a subset of another list
+    # @list1: shorter list
+    # @list2: longer list
+    # return true if list1 is a subset of list2, otherwise false
+    def isSubset(self, list1, list2):
+        return all(elem in list2 for elem in list1)
+
+    # process MM JSON output
+    # @output: MM output, string type
+    # @types: list of semantic types
+    # return the max scored item
+    # if it has more than one max scored item, return the item with more MM types
+    # TODO: need to rewrite this part to avoid nested loop
+
     def selectOutput(self, output, types):
+        mm_types = self.readTypes(types)
         mm_output = json.loads(output)
-        docs = mm_output["AllDocuments"] ## must be []
+        docs = mm_output["AllDocuments"]  # must be []
         for doc in docs:
             document = doc["Document"]
-            utterances = document["Utterances"] ## must be []
+            utterances = document["Utterances"]  # must be []
             for each in utterances:
-                phrases = each["Phrases"] ## must be []
-                for item in phrases:
-                    mappings = item["Mappings"] ## must be []
-                    for anno in mappings:
-                        print(anno)
+                phrases = each["Phrases"]  # must be []
+                for phrase in phrases:
+                    mappings = phrase["Mappings"]  # must be []
+                    for item in mappings:
+                        candidates = item["MappingCandidates"]  # must be []
+                        # get scores
+                        scores = [int(can["CandidateScore"])
+                                  for can in candidates]
+                        # find max-scored index
+                        max_index = [index for index, val in enumerate(
+                            scores) if val == max(scores)]
+                        # find item with max_index
+                        max_candidate = [itemgetter(*max_index)(candidates)]
+                        # if a term has multiple records, keep the one with more types
+                        print(max_candidate)
                         print("\n")
-    ## HDI annotation process
-    ## get HDI content annotated using MM
-    ## @name: herb name that in the overlap
-    ## @hdi: HDI content for the herb
-    ## @mm: MetaMap constructor
-    ## @output_file: the local file name to write
+
+    # HDI annotation process
+    # get HDI content annotated using MM
+    # @name: herb name that in the overlap
+    # @hdi: HDI content for the herb
+    # @mm: MetaMap constructor
+    # @output_file: the local file name to write
+
     def HDIprcess(self, name, hdi, mm, output_file):
         data = {}
         data["name"] = name
-        hdi = self.remove(self.getBefore(hdi))
-        ## if hdi is empty
-        if not hdi:
+        content = self.remove(self.getBefore(hdi))
+        # if hdi is empty
+        if not content:
             data["HDI"] = " "
             data["annotated_HDI"] = " "
-        ## hdi is not empty
+        # hdi is not empty
         else:
-            if isinstance(hdi, list):
-                for each in hdi:
-                    command = mm.getComm(each, additional = " --term_processing")
-                    print(command)
+            if isinstance(content, list):
+                for each in content:
+                    command = mm.getComm(each, additional=" --term_processing")
                     output = mm.getAnn(command, "JSON").decode("utf-8")
-                    ## remove output first line
+                    # remove output first line
                     output = "\n".join(output.split("\n")[1:])
-                    self.selectOutput(output)
+                    self.selectOutput(output, "HDI")
             else:
-                command = mm.getComm(hdi, additional = " --term_processing")
+                command = mm.getComm(content, additional=" --term_processing")
                 output = mm.getAnn(command, "JSON").decode("utf-8")
-                self.selectOutput(output)
+                output = "\n".join(output.split("\n")[1:])
+                self.selectOutput(output, "HDI")
         #self.writeContent(output_file, data)
-    ## AR annotation process
-    ## get AR content annotated using MEDDRA
-    ## @name: herb name
-    ## @ar: AR content
-    ## @meddra: MEDDRA constructor
-    ## @output_file: the local file name to write
+    # AR annotation process
+    # get AR content annotated using MEDDRA
+    # @name: herb name
+    # @ar: AR content
+    # @meddra: MEDDRA constructor
+    # @output_file: the local file name to write
+
     def ADRprocess(self, name, ar, meddra, output_file):
         data = {}
         data["name"] = name
         print(name)
         ar = self.remove(self.concate(ar, " "))
-        ## if ar is empty
+        # if ar is empty
         if not ar:
             data["ADR"] = " "
             data["annotated_ADR"] = " "
@@ -129,23 +165,24 @@ class main(object):
             data["ADR"] = ar
             data["annotated_ADR"] = res
         self.writeContent(output_file, data)
-    ## purposed uses annotation process
-    ## get purposed uses content annotated using MM
-    ## @name: herb name that in the overlap
-    ## @pu: contraindications content for the herb
-    ## @mm: MetaMap constructor
-    ## @output_file: the local file name to write
+    # purposed uses annotation process
+    # get purposed uses content annotated using MM
+    # @name: herb name that in the overlap
+    # @pu: contraindications content for the herb
+    # @mm: MetaMap constructor
+    # @output_file: the local file name to write
+
     def PUprpcess(self, name, pu, mm, output_file):
         data = {}
         data["name"] = name
         pu = self.remove(self.concate(pu))
-        ## if pu is empty
+        # if pu is empty
         if not pu:
             data["PU"] = " "
             data["annotated_PU"] = " "
-        ## hdi is not empty
+        # hdi is not empty
         else:
-            command = mm.getComm(pu, additional = "--term_processing")
+            command = mm.getComm(pu, additional="--term_processing")
             output = mm.getAnnNoOutput(command).decode("utf-8")
             print("=======================================")
             print(name)
@@ -160,62 +197,72 @@ class main(object):
             data["annotated_PU"] = res
             data["PU"] = pu
             print("=======================================")
-    ## read MM type file
-    ## @fun: annotation section names, i.e. PU, HDI
-    ## each section will return a list of MM types
+    # read MM type file
+    # @fun: annotation section names, i.e. PU, HDI
+    # each section will return a list of MM types
+
     def readTypes(self, fun):
-        full_types = pd.read_csv("mmmap.txt", sep = "|", header = None, index_col = False)
+        full_types = pd.read_csv(os.path.join(self.file_location, "mmmap.txt"), sep="|",
+                                 header=None, index_col=False)
         full_types.columns = ["abbrev", "tui", "types"]
         if fun.upper() not in ["HDI", "PU", "CON"]:
             raise ValueError("Currently only supports HDI, PU and Con.")
         else:
-            ## hdi mm types
+            # hdi mm types
             if fun.upper() == "HDI":
-                hdi_types = pd.read_csv("hdi_types.txt", sep = "|", header = None, index_col = False)
+                hdi_types = pd.read_csv(
+                    os.path.join(self.file_location, "hdi_types.txt"), sep="|", header=None, index_col=False)
                 hdi_types.columns = ["group", "group_name", "tui", "types"]
-                hdi_types = hdi_types["tui"].values()
+                hdi_types = hdi_types["tui"].values
                 hdi = full_types.loc[full_types["tui"].isin(hdi_types)]
-                return hdi["abbrev"].values()
-            ## pu mm types
+                return hdi["abbrev"].values
+            # pu mm types
             if fun.upper() == "PU":
-                pu_types = pd.read_csv("pu_types.txt", sep = "|", header = None, index_col = False)
+                pu_types = pd.read_csv(
+                    os.path.join(self.file_location, "pu_types.txt"), sep="|", header=None, index_col=False)
                 pu_types.columns = ["group", "group_name", "tui", "types"]
-                pu_types = pu_types["tui"].values()
+                pu_types = pu_types["tui"].values
                 pu = full_types.loc[full_types["tui"].isin(pu_types)]
-                return pu["abbrev"].values()
-            ## con mm types
+                return pu["abbrev"].values
+            # con mm types
             if fun.upper() == "CON":
-                con_types = pd.read_csv("con_types.txt", sep = "|", header = None, index_col = False)
+                con_types = pd.read_csv(
+                    os.path.join(self.file_location, "con_types.txt"), sep="|", header=None, index_col=False)
                 con_types.columns = ["group", "group_name", "tui", "types"]
-                con_types = con_types["tui"].values()
+                con_types = con_types["tui"].values
                 con = full_types.loc[full_types["tui"].isin(pu_types)]
-                return con["abbrev"].values()
-    ## read the herb file
+                return con["abbrev"].values
+    # read the herb file
+
     def readFile(self):
-        ## start mm server
+        # start mm server
         mm = umlsAnn()
         mm.start()
-        ## get meddra constructor
+        # get meddra constructor
         meddra = meddraAnn()
         with open(os.path.join(self.file_location, self.read_file), "r") as f:
             for line in f:
                 herb = json.loads(line)
                 if herb["name"] in self.overlap_herbs:
-                    #pass
-                    ## HDI
-                    self.HDIprcess(herb["name"], herb["herb-drug_interactions"], mm, "overlap_hdi.tsv")
+                    # pass
+                    # HDI
+                    self.HDIprcess(
+                        herb["name"], herb["herb-drug_interactions"], mm, "overlap_hdi.tsv")
                     break
                     #self.writeContent("overlap_hdi.tsv", data)
-                    ## ADR
+                    # ADR
                     #self.ADRprocess(herb["name"], herb["adverse_reactions"], meddra, "overlap_adr.tsv")
-            
+
                     ##self.PUprpcess(herb["name"], herb["purported_uses"], mm, "overlap_con.tsv")
                 else:
                     pass
-    ## main function
+    # main function
+
     def run(self):
-        #self.readTypes()
+        # self.readTypes()
         self.readFile()
+
+
 if __name__ == "__main__":
     x = main()
     x.run()
--- CANCER/meddraAnn.py
+++ CANCER/meddraAnn.py
@@ -1,36 +1,51 @@
-import urllib.request, urllib.error, urllib.parse
-import json, config, os, subprocess, re, csv, pickle
+import urllib.request
+import urllib.error
+import urllib.parse
+import json
+import config
+import os
+import subprocess
+import re
+import csv
+import pickle
+
+
 class meddraAnn(object):
     def __init__(self):
-        self.meddra = "/annotator?ontologies=http://data.bioontology.org/ontologies/MEDDRA&text=" 
+        self.meddra = "/annotator?ontologies=http://data.bioontology.org/ontologies/MEDDRA&text="
         self.REST_URL = "http://data.bioontology.org"
         self.API_KEY = config.api_key
-    ## BioPortal login
+    # BioPortal login
+
     def auth(self, url):
         opener = urllib.request.build_opener()
         opener.addheaders = [('Authorization', 'apikey token=' + self.API_KEY)]
         return json.loads(opener.open(url).read())
-    ## get prefLabel from annotation
-    ## @ annotations: the annotated document from BioPortal
-    def getLabel(self, annotations, get_class = True):
+    # get prefLabel from annotation
+    # @ annotations: the annotated document from BioPortal
+
+    def getLabel(self, annotations, get_class=True):
         labels = []
         for result in annotations:
             class_details = result["annotatedClass"]
             if get_class:
                 try:
-                    class_details = self.auth(result["annotatedClass"]["links"]["self"])
+                    class_details = self.auth(
+                        result["annotatedClass"]["links"]["self"])
                 except urllib.error.HTTPError:
                     print(f"Error retrieving {result['annotatedClass']['@id']}")
                     continue
             labels.append(class_details["prefLabel"])
         return labels
-    ## adverse reactions pre-process
-    ## @ar: data["adverse_reactions"]
-    ## return annotated terms
+    # adverse reactions pre-process
+    # @ar: data["adverse_reactions"]
+    # return annotated terms
+
     def adrProcess(self, ar):
-        ## check if exists adverse_reactions section
+        # check if exists adverse_reactions section
         if ar:
-            ar_annotations = self.auth(self.REST_URL + self.meddra + urllib.parse.quote(ar))
+            ar_annotations = self.auth(
+                self.REST_URL + self.meddra + urllib.parse.quote(ar))
             labels = self.getLabel(ar_annotations)
             return labels
         else:
